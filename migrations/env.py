e3b9a2e10753171975015d63279e5686406b3a47
victor <victorsoaresferreira09@gmail.com>
2024-07-01 18:20:17 -0300

    mapeando a banco de dados

--- migrations/env.py
+++ migrations/env.py
@@ -5,9 +5,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from fast_zero.models import table_registry
+from fast_zero.settings import Settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = table_registry.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
